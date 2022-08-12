666e9a917eea009b3c0b442ebd7a62fce0893e09
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-08-12 08:34:47 -0300

    Possible moves of piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -12,4 +12,23 @@ public abstract class Piece {
 	protected Board getBoard() {
 		return board;
 	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] possibleMoves = possibleMoves();
+		
+		for (int i = 0; i < possibleMoves.length; i++) {
+			for (int j = 0; j < possibleMoves.length; j++) {
+				if (possibleMoves[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
